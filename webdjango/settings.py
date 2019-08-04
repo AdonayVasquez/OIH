c748dd182768af18c133d311c617ca05335504f0
adonay <adonay16.vz@gmail.com>
2019-08-03 21:49:32 -0600

    cambios en disenio de articulos

--- webdjango/settings.py
+++ webdjango/settings.py
@@ -27,7 +27,7 @@ STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'),)
 SECRET_KEY = 'xm=3-z5t#(8q192!f_ubx3fa8%193f$v$x7q@g+h&l)=*_g_wf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['165.22.214.38']
 
